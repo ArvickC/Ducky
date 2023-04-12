0cab8895d12f54675a68df8b5ac33759b1aaa392
arvickc <arvick@outlook.com>
2023-04-11 23:30:40 -0700

    Initial commit

---
+++ src/main/java/net/mystc/ducky/DuckyClient.java
@@ -0,0 +1,10 @@
+package net.mystc.ducky;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DuckyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
